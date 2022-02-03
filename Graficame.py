affffb8ba44177e89eb5a172978afca119e95cc6
Gwerr1002 <98936739+Gwerr1002@users.noreply.github.com>
2022-02-03 00:58:02 -0600

    Update Graficame.py

--- Graficame.py
+++ Graficame.py
@@ -10,6 +10,21 @@ import scipy.fft as spf #Grafica el espectro de una señal
 import numpy as np #manejo de arreglos
 
 class Graf():
+    '''
+        Clase Graf(t,x1,x2,nombre) Se utiliza para graficar señales de audio. Recibe dos canales si es estéreo
+        un nombre y una linea de tiempo
+
+        Parameters
+        ----------
+        t      : array
+                 Eje de tiempo del registro
+        x1     : array
+                 Canal 1 a graficar
+        x2     : array
+                 Canal 2 a graficar
+        nombre : string
+                 Nombre del archivo
+    '''
     def __init__(self,t,x1,x2,nombre):
         
         self.t = t
@@ -18,6 +33,17 @@ class Graf():
         self.nombre = nombre
         
     def Muestra(self, x, etiqueta):
+        """
+            Graf.Muestra(x, etiqueta) Grafica un registro
+            
+            Parameters
+            ----------
+            x        : array
+                       Registro a graficar
+            
+            etiqueta : string
+                       Nombre o identificador del registro
+        """
         
         plt.figure(figsize=(15, 5))
         plt.plot(self.t, x, color = 'r', label = etiqueta)
@@ -28,6 +54,14 @@ class Graf():
         plt.legend();
     
     def GrafEspectro(self,fs):
+        """
+            Grafica el espectro de la señal del canal1 con la FFT Graf.GrafEspectro(fs)
+            
+            Parameters
+            ----------
+            fs : int
+                 Frecuencia de muestreo
+        """
         
         N = len(self.x1)
         X = spf.fft(self.x1)
@@ -44,6 +78,14 @@ class Graf():
         
     
     def GrafVista(self, canal):
+        """
+            Graf.GrafVista(canal) muestra en pantalla el canal que se recibe, utiliza el metodo Muestra de esta clase
+            
+            Parameters
+            ----------
+            canal : array
+                    registro a graficar
+        """
         
         if canal == 1:
             etiqueta = "CH1"
@@ -55,9 +97,14 @@ class Graf():
         self.Muestra(x,etiqueta + " $x10^3$")
         
     def GrafSumaCh(self):
+        "Noimplementado"
         self.Muestra((self.x1 + self.x2)/1000, "Suma ch1 + ch2 $x10^3$")
         
     def GrafPotencia(self):
+        """
+            Graf.GrafPotencia() grafica la potencia de la señal sumando los canales, reflejando la parte negativa. Posteriormente se eleva al cuadrado
+        """
+        
         xc = self.x1 + self.x2
         
         x = xc*(xc >= 0)/1000
@@ -65,12 +112,5 @@ class Graf():
         y = xc*(xc <= 0)/1000
         y = (-1)*np.power(y,2)
         xc = x+y
-        
-        '''
-        y = (xc*(xc <= 0))**2
-        y = y*(-1)
-        xc = x + y
-        '''
-        
         self.Muestra(xc, "Potencia en $Megas x10^6$ (parte negativa añadida y reflejada)")
         
